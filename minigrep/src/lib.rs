7d9a472c60fe5756069121e19d60099a5ea0593b
jamesjmtaylor <james.jm.taylor@gmail.com>
2023-01-18 07:06:10 -0800

    Completed minigrep cli program.  Added unit tests to validate behavior.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,26 +1,114 @@
+use std::env;
 use std::fs;
 use std::error::Error;
 
 pub struct Config {
-    query: String,
-    file_path: String
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() != 3 {
-            return Err("You must provide exactly 2 inputs (query and filename)");
+        if args.len() < 3 {
+            return Err("You must provide at least 2 inputs (query and filename)");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Searching for {} in file {}", config.query, config.file_path);
-
     let contents = fs::read_to_string(config.file_path)?;
-    println!("{contents}");
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for line in results {
+        println!("{line}")
+    }
     Ok(())
+}
+
+/**
+ * Borrows a lifetime reference and searches it for the given string reference.
+ * Returns a vector with all lifetime string references tied to the input 
+ * lifetime. In other words, we tell Rust that the data returned by the search
+ * function will live as long as the data passed into the search function in the
+ * contents argument. 
+ */
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+// Tests module
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        // `\` character escapes the newline character in the string
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."], 
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "Duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["Duct tape."], 
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,11 +6,13 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect(); // Turns iterator into Vector
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        //NOTE: `eprintln!` outputs to stderror. `println!` to stdoutput
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
+
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
