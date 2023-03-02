332cec569ef5b2d182fd043fd99f815679fbe23d
jamesjmtaylor <james.jm.taylor@gmail.com>
2023-03-02 09:09:06 -0800

    main Removed nested rust project structure so that rust-analyzer could work properly.

--- src/main.rs
+++
@@ -1,6 +0,0 @@
-// `const` is always immutable, must have an explicit type, and can be in any scope
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-
-fn main() {
-    println!("Hello, world!");
-}
